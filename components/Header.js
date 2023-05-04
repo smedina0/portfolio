49e45016fc6d0db4aafae54dc276f64c379dee3a
Steven Medina <smedina2494@gmail.com>
2023-05-03 21:16:14 -0400

    WIP: Adds header styles

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
 import Image from 'next/image';
+import styles from '@/styles/Header.module.css';
 
 function Header() {
   return (
-    <header>
-      {/* Add image tag here */}
-      <h1>Hi, I'm Steven Medina</h1>
+    <header className={`${styles.header} flex-ctr-centered`}>
+      <div className='flex-ctr-centered'>
+        <Image
+          src='/steven-medina.jpg'
+          height={200}
+          width={200}
+          alt='Steven Medina'
+        />
+        <h1>Hi, I'm Steven Medina</h1>
+      </div>
     </header>
   );
 }
