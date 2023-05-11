2912293be658ac54ca5e9f2492c4e85c93a5b7d5
Steven Medina <smedina2494@gmail.com>
2023-05-10 23:51:44 -0400

    WIP: Updates page styles

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,22 @@ import styles from '@/styles/Header.module.css';
 function Header() {
   return (
     <header className={`${styles.header} flex-ctr-centered`}>
-      <div className='flex-ctr-centered'>
+      <div className={`${styles.nameAndImage} flex-ctr-centered`}>
         <Image
           src='/steven-medina.jpg'
           height={200}
           width={200}
           alt='Steven Medina'
         />
-        <h1>Hi, I'm Steven Medina</h1>
+        <div>
+          <h1>Hi, I'm Steven Medina</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+            veniam quas. Pariatur, sed nulla eaque dicta doloremque magnam
+            laudantium ut, odio iste, quibusdam quo unde ducimus porro totam
+            temporibus quaerat!
+          </p>
+        </div>
       </div>
     </header>
   );
